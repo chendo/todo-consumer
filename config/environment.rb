2155a2a04c2cda03e3c2b970cd5f9a530b3f9693
Dr Nic Williams <drnicwilliams@gmail.com>
2010-05-14 08:27:58 +1000

    restrict inher_res to 1.0.3

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-  config.gem 'inherited_resources'
+  config.gem 'inherited_resources', :version => '1.0.3'
   config.gem 'haml'
   config.gem 'formtastic'
   config.gem 'devise', :version => '1.0.7'
