0ef9b46f210c5dcbe03ae71a3fd23072167b9cb7
Jack Chen <chendo@chendo.net>
2010-05-13 15:15:28 +1000

    Fixed another local referrig path

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,7 @@ class DashboardController < ApplicationController
   def index
     if current_user
       begin
-        response = current_user.todo_token.client.get("http://todo.local/todo_items.json")
+        response = current_user.todo_token.client.get("http://todo-provider.heroku.com/todo_items.json")
         @items = JSON.parse(response.body)
       rescue JSON::ParserError => e
         @items = ["No JSON returned"]
